c500136200d729d814e58db80af36c8b8fc5f54c
Adediran babatomiwa <babatomm@gmail.com>
2025-08-23 13:23:13 +0100

    tom

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-// src/App.tsx
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/home/Home";
-import Listing from "./pages/listingpage/Listing"
+import Listing from "./pages/listingpage/Listing";
 
-const App: React.FC = () => {
-  return (
-    <div>
-      {/* Page Routes */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/listing" element={<Listing />} />
-      </Routes>
-    </div>
-  );
-};
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/listing", element: <Listing /> },
+]);
 
-export default App;
+export default function App() {
+  return <RouterProvider router={router} />;
+}
