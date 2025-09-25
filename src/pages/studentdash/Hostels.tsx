3f652f4fa37273b7c1a961511ecce4db16a93b02
Adediran babatomiwa <babatomm@gmail.com>
2025-09-25 21:25:42 +0100

    tom

--- src/pages/studentdash/Hostels.tsx
+++ src/pages/studentdash/Hostels.tsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
 import clsx from "clsx";
 import { BsQuestionCircle } from "react-icons/bs";
-import { GrStatusGood } from "react-icons/gr";
 import InfoPill from "../../components/Pill";
 import {
   MdOutlinePending,
-  MdWoman2,
   MdOutlinePostAdd,
   MdLightbulbOutline,
 } from "react-icons/md";
@@ -15,17 +13,10 @@ import { HiOutlineUserCircle } from "react-icons/hi";
 import { FaArrowRight, FaToggleOn } from "react-icons/fa";
 import { FiChevronDown } from "react-icons/fi";
 import { MdOutlineDeleteForever } from "react-icons/md";
-import { PiHouse } from "react-icons/pi";
-import { HiOutlineUsers, HiOutlineDotsVertical } from "react-icons/hi";
-import {
-  MdOutlinePendingActions,
-  MdErrorOutline,
-  MdBlock,
-} from "react-icons/md";
+import { HiOutlineDotsVertical } from "react-icons/hi";
 import { FaPlus } from "react-icons/fa";
-import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
-import { ChevronRightCircle } from "lucide-react";
-import { RiInformation2Fill, RiInformationLine } from "react-icons/ri";
+import { IoIosArrowBack } from "react-icons/io";
+import { RiInformationLine } from "react-icons/ri";
 
 // ----------------------- Reusable Label -----------------------
 type LabelProps = React.PropsWithChildren<{ className?: string }>;
